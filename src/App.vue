7ee62f85413e61153a19fe591827ac4742600269
Mateus Mesko <58198358+mateusmesko@users.noreply.github.com>
2021-08-25 20:58:08 -0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,67 @@
+<template>
+	<div id="app">
+		<h1>Filtros  Mixins</h1>
+	<hr>
+	<P>Cpf sem valor filtro::{{cpf}}</P>
+	<p>Cpf com valor filtro: {{cpf |cpfFilter}}</p>
+	<p>teste mixin: {{usuarioLogado}}</p>
+	<p>Cpf com valor filtro+invertido: {{cpf |cpfFilter|inverter}}</p>
+	<hr>
+	<h1>teste de inputs</h1>
+	<input type="text" :value="cpf|cpfFilter|inverter">
+	<hr>
+	<Frutas />
+	 <div>
+        <ul>
+            <li v-for="fruta in frutas" :key="fruta">{{fruta}}</li>
+        </ul>
+        <input type="text" v-model="fruta" @keydown.enter="add">
+    </div>
+	</div>
+	
+</template>
+
+<script>
+import frutasMixin from './frutasMixin'
+import usuarioMixin from './UsuarioMixin'
+import Frutas from './Frutas.vue'
+
+export default {
+	components:{ Frutas },
+	mixins:[frutasMixin,usuarioMixin],
+	filters:{
+		cpfFilter(valor)
+		{
+			const arr= valor.split('')
+			arr.splice(3,0,'.')
+			arr.splice(7,0,'.')
+			arr.splice(11,0,'-')
+			return arr.join('')
+		}
+	},
+	data()
+	{
+		return{
+			cpf:'12345678909'
+		}
+	},
+        created(){
+            console.log('created- app.vue')
+        }
+}
+</script>
+
+<style>
+#app {
+	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+	font-size: 2.5rem;
+}
+input{
+	font-size: 2.5rem
+}
+</style>
---
+++ src/UsuarioMixin.js
@@ -0,0 +1,11 @@
+export default{
+    computed:{
+        usuarioLogado()
+        {
+            return 'marisa Silva'
+        },
+        created(){
+            console.log('created- usuario mixin')
+        }
+    }
+}
